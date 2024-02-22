a5c0a342b2639d2b0ebc269e397a81b9c59d91a2
bitwisexor <AACOSTA179@student.glendale.edu>
2024-02-21 19:59:29 -0800

    button rework
    Removed placeholder buttons -- still missing "start" button sprite. Options and quit are now added as sprites, hooked up to button objects.

--- Button.gd
+++ Button.gd
@@ -1,4 +1,4 @@
-extends Control
+extends Button
 
 
 # Called when the node enters the scene tree for the first time.
@@ -9,14 +9,3 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
-
-
-func _on_quit_pressed():
-	get_tree().quit() # Tells the game to exit if "Quit" is clicked
-
-func _on_optionsbutton_pressed():
-	get_node("burning").play()
-
-
-func _on_quitbutton_pressed():
-	get_tree().quit()
