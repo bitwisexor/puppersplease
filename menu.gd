1fa13efee9e3def151b0a010660d3a7755337b36
bitwisexor <AACOSTA179@student.glendale.edu>
2024-03-14 05:42:12 -0700

    Dialogic addon integrated
    Folder restructure, switched to Dialogic for dialogue

--- menu.gd
+++ menu.gd
@@ -5,8 +5,7 @@ signal clicked_play
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
-
-
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
@@ -16,12 +15,13 @@ func _on_quit_pressed():
 	get_tree().quit() # Tells the game to exit if "Quit" is clicked
 
 func _on_optionsbutton_pressed():
-	get_node("burning").play()
-
+	pass
 
 func _on_quitbutton_pressed():
 	get_tree().quit() # Exit game if quit is pressed
 
 
 func _on_startbutton_pressed():
+	Fade.fade_out(1, Color.BLACK, "Diamond", false, false)
 	emit_signal("clicked_play")
+
